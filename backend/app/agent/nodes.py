7ec7119547e88ad34a9619f9935374874824f6db
anshhh <sharmaansh8140@gmail.com>
2026-03-01 18:35:46 +0530

    Added agent nodes and resume parser logic and made prompt for resumea-parser

---
+++ backend/app/agent/nodes.py
@@ -0,0 +1,22 @@
+from langchain_groq import ChatGroq
+from prompts import resume_parser_prompt
+from state import AgentState
+from pypdf import PdfReader
+from dotenv import load_dotenv
+import os
+load_dotenv()
+pdf_path=os.getenv("RESUME_PATH")
+model=ChatGroq(model="llama-3.1-8b-instant")
+def resume_parser(state:AgentState):
+    reader=PdfReader(pdf_path)
+    resume_text=""
+    for page in reader.pages:
+        resume_text+=page.extract_text()
+    prompt = resume_parser_prompt.format(resume_text=resume_text)
+    result = model.invoke(prompt)
+    print(result.content)
+    return{"resume":result.content} 
+   
+# if __name__ == "__main__":          
+#     result = resume_parser({})    ## using it only for testing and debuggin 
+#     print(result)                        
---
+++ backend/app/agent/prompts.py
@@ -0,0 +1,15 @@
+from langchain_core.prompts import PromptTemplate
+resume_parser_prompt = """
+You are a resume parser. Extract the following from this resume text:
+-Education
+- skills
+- projects
+- experience
+- university
+-nachievements
+
+Return as JSON only with proper spacing . No extra text.
+recheck and scan whole text before actually giving the output so no important things get replaced
+Resume:
+{resume_text}
+"""
